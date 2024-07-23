04761542eef35ba8d82f6c38f744347535283bf0
Adi Cohen <adicohen96@me.com>
2024-07-23 21:23:56 +0300

    added caching to the NasdaqAPI

--- src/NasdaqAPI.py
+++ src/NasdaqAPI.py
@@ -2,24 +2,86 @@ import time
 import requests
 import random
 import json
+import os
 import pandas as pd
 from src.Limiter import RateLimiter
 from typing import List, Union
+from datetime import datetime
+import pytz
 
 REQUESTS_PER_SECOND = 3
+CACHE_FILE_PATH = '/Users/adic/Desktop/Projects/stock_analysis/src/cache.json'
+HEADERS_PATH = '/Users/adic/Desktop/Projects/stock_analysis/src/headers.json'
+
 
 class NasdaqAPI:
 
-    def __init__(self, headers_file='/Users/adic/Desktop/Projects/stock_analysis/src/headers.json'):
+    def __init__(self, headers_file = HEADERS_PATH):
         self.base_url = 'https://api.nasdaq.com/api'
+
+        if not os.path.exists(headers_file):
+            raise FileNotFoundError(f"Headers file not found: {headers_file}")
         with open(headers_file, 'r') as file:
             self.headers_list = json.load(file)
+        if not self.headers_list:
+            raise ValueError("Headers list is empty. Please provide valid headers.")
+
+            # Load the cache file if it exists
+        if os.path.exists(CACHE_FILE_PATH):
+            try:
+                with open(CACHE_FILE_PATH, 'r') as cache_file:
+                    # Attempt to load the cache
+                    self.cache = json.load(cache_file)
+                    # Check if the cache is empty
+                    if not self.cache:
+                        print("Cache file is empty. Initializing empty cache.")
+                        self.cache = {}
+            except json.JSONDecodeError:
+                # Handle cases where the file is not a valid JSON
+                print("Cache file is not a valid JSON. Initializing empty cache.")
+                self.cache = {}
+        else:
+            self.cache = {}
+
+    def save_cache(self):
+        with open(CACHE_FILE_PATH, 'w') as cache_file:
+            json.dump(self.cache, cache_file, indent = 4) 
+
+    def __is_cache_valid(self, ticker):
+        if ticker not in self.cache:
+            return False
+        
+        cache_date = self.cache[ticker].get('date')
+        if not cache_date:
+            return False
+
+        # Convert cache_date to datetime object
+        cache_date = datetime.strptime(cache_date, '%Y-%m-%d')
+        eastern = pytz.timezone('US/Eastern')
+        now = datetime.now(eastern)
+        
+        # Check if the cache is for today and if the stock market is still open
+        if cache_date.date() == now.date() and now.time() < datetime.strptime('16:00', '%H:%M').time():
+            return True
+        
+        # Check if the cache is from Friday and today is Saturday, Sunday, or Monday before 16:00 Eastern time
+        if cache_date.weekday() == 4:  # Friday
+            if now.weekday() in [5, 6] or (now.weekday() == 0 and now.time() < datetime.strptime('16:00', '%H:%M').time()):
+                return True
+        
+        return False   
 
     def get_random_headers(self):
+        if not self.headers_list:
+            raise ValueError("Headers list is empty. Please provide valid headers.")
         return random.choice(self.headers_list)
 
     def _fetch_basic_info(self, tickers: List[str]) -> pd.DataFrame:
         def fetch_single_basic_info(ticker):
+
+            if self.__is_cache_valid(ticker):
+                return self.cache[ticker]
+
             url = f'{self.base_url}/quote/{ticker}/summary?assetclass=stocks'
             headers = self.get_random_headers()
             try:
@@ -27,14 +89,18 @@ class NasdaqAPI:
                 response.raise_for_status()
                 data = response.json()
                 summary_data = data.get('data', {}).get('summaryData', {})
-                return {
+                info = {
                     'Ticker': ticker,
                     'PreviousClose': summary_data.get('PreviousClose', {}).get('value', 'Not available'),
                     'MarketCap': summary_data.get('MarketCap', {}).get('value', 'Not available'),
                     'Sector': summary_data.get('Sector', {}).get('value', 'Not available'),
                     'Industry': summary_data.get('Industry', {}).get('value', 'Not available'),
-                    'Exchange': summary_data.get('Exchange', {}).get('value', 'Not available')
+                    'Exchange': summary_data.get('Exchange', {}).get('value', 'Not available'),
+                    'date': datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d')
                 }
+                self.cache[ticker] = info
+                return info
+            
             except requests.exceptions.HTTPError as http_err:
                 print(f"HTTP error occurred: {http_err}")
                 return {
@@ -43,8 +109,10 @@ class NasdaqAPI:
                     'MarketCap': 'Error fetching data',
                     'Sector': 'Error fetching data',
                     'Industry': 'Error fetching data',
-                    'Exchange': 'Error fetching data'
+                    'Exchange': 'Error fetching data',
+                    'date': datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d')
                 }
+            
             except Exception as err:
                 print(f"An error occurred: {err}")
                 return {
@@ -53,12 +121,11 @@ class NasdaqAPI:
                     'MarketCap': 'Error fetching data',
                     'Sector': 'Error fetching data',
                     'Industry': 'Error fetching data',
-                    'Exchange': 'Error fetching data'
+                    'Exchange': 'Error fetching data',
+                    'date': datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d')
                 }
 
         rate_limiter = RateLimiter(requests_per_second = REQUESTS_PER_SECOND)
-        # basic_info_list = [rate_limiter.apply(fetch_single_basic_info, ticker) for ticker in tickers]
-
         basic_info_list = []
 
         for i, ticker in enumerate(tickers):
@@ -68,8 +135,11 @@ class NasdaqAPI:
                 time.sleep(5)
             
             info = rate_limiter.apply(fetch_single_basic_info, ticker)
-            basic_info_list.append(info)        
+            basic_info_list.append(info)  
 
+        # Save the cache after fetching data
+        self.save_cache()
+        del rate_limiter
         return pd.DataFrame(basic_info_list)
 
     def get_basic_info(self, tickers: Union[str, List[str]]) -> pd.DataFrame:
@@ -101,11 +171,12 @@ class NasdaqAPI:
 
 def main():
     api = NasdaqAPI()
-    tickers = ['AAPL', 'AMZN','MSFT','TSLA','UPRO']
-    info = api.get_basic_info('PLTR')
+    tickers = ['AAPL', 'AMZN','MSFT','TSLA']
+    info = api.get_basic_info(tickers)
     print(info)
-    info2 = api.get_market_cap('QQQ')
-    print(info2)
+
+    # info2 = api.get_market_cap('QQQ')
+    # print(info2)
 
 
     #market_caps = api.get_market_cap(tickers, requests_per_second=1)
